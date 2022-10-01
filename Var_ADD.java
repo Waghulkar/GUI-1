4ed9353d85e5fa315aff220515eeedd6ef4911f8
Waghulkar <poojadhawade@gmail.com>
2022-10-01 19:59:13 +0530

    CHANGE DONE

---
+++ Var_ADD.java
@@ -0,0 +1,17 @@
+package TESTING;
+
+public class Var_ADD
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		int a=10;
+			System.out.println("enter the value of integer"+a);
+		float b=20.68f;
+		    System.out.println("enter the value of float" +b);
+	}  
+
+}
+//THIS IS MY ADDITION LOCAL METHOD
